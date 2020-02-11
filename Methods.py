47ef0bc7582d7937c641b634df7bd683b1f2426c
alexr <belloa@hartwick.edu>
2020-02-11 10:00:33 -0500

    2/11/2020

--- Methods.py
+++ Methods.py
@@ -1,1 +1,7 @@
+#Alex Bello
+#2/11/2020
+#The list of methods used for the Operation project.
+
+
+
 
--- Operation.py
+++ Operation.py
@@ -1,1 +1,25 @@
+#Alex Bello
+#2/11/2020
+#Applies different methods to a user generated list that do not modify the original list
+
+import collections
+
+
+#Creating a blank list for a user to input into
+ages = []
+#This will do multiple things, have the user pick how many numbers they want for the list, the numbers they want in the
+#list, and puts those numbers into the blank list
+n = int(input("Please input how many numbers you want in this list, thanks! :"))
+for i in range(0 , n):
+    print("Enter number at location", i, ":")
+    item = int(input())
+    ages.append(item)
+print("Your list is", ages)
+
+sorted(ages)
+ages
+
+
+
+
 
