07369449509cb25c17d30abd1e8f8bedf1dec645
alexr <belloa@hartwick.edu>
2020-02-12 12:04:30 -0500

    2/12/2020

--- Methods.py
+++ Methods.py
@@ -1,7 +1,87 @@
-#Alex Bello
-#2/11/2020
-#The list of methods used for the Operation project.
+# Alex Bello
+# 2/11/2020
+# The list of methods used for the Operation project.
 
 
+def sums(lists):
+    sum = 0
+    for i in lists:
+        sum += i
+    return sum
 
 
+def product(lists):
+    total = 0
+    for i in lists:
+        total *= i
+    return total
+
+
+def mean(lists):
+    avg = sums(lists)
+    return avg / len(lists)
+
+
+def median(lists):
+    mid = (len(lists) - 1) // 2
+    if mid % 2 == 1:
+        mid = lists[mid] + lists[mid + 1] / 2
+    else:
+        mid = lists[mid]
+    return mid
+
+
+def mode(lists):
+    mode = []
+    count = 0
+    for i in range(len(lists)):
+        temp_count = 1
+        for j in range(1, len(lists)):
+            if lists[i] == lists[j]:
+                temp_count += 1
+        if temp_count > count:
+            mode.clear()
+            count = temp_count
+        if temp_count >= count:
+            mode.append(lists[i])
+    return set(mode)
+
+
+def big_boi(lists):
+    large = lists[0]
+    for i in lists:
+        if large < i:
+            large = i
+    return large
+
+
+def small_boi(lists):
+    little = lists[0]
+    for i in lists:
+        if little > i:
+            little = i
+    return little
+
+
+def removal(lists):
+    return list(set(lists))
+
+
+def odd_times(lists):
+    return [odd for odd in lists if odd % 2 == 1]
+
+
+def even_times(lists):
+    return [even for even in lists if even % 2 == 0]
+
+
+def tell_me(lists):
+    new_num = int(input("Give me a number boyo! "))
+    for i in lists:
+        if i == new_num:
+            return True
+    return False
+
+
+def bonus(lists):
+    return lists[-2]
--- Operation.py
+++ Operation.py
@@ -2,24 +2,32 @@
 #2/11/2020
 #Applies different methods to a user generated list that do not modify the original list
 
-import collections
+from Methods import *
 
 
 #Creating a blank list for a user to input into
 ages = []
 #This will do multiple things, have the user pick how many numbers they want for the list, the numbers they want in the
 #list, and puts those numbers into the blank list
-n = int(input("Please input how many numbers you want in this list, thanks! :"))
+n = int(input("Please input how many numbers you want in this list, thanks! : "))
 for i in range(0 , n):
-    print("Enter number at location", i, ":")
-    item = int(input())
+    item = int(input("Enter number at location " + str(i) + " : "))
     ages.append(item)
 print("Your list is", ages)
-
-sorted(ages)
-ages
-
-
+ages.sort()
+print(ages)
+print(sums(ages))
+print(product(ages))
+print(mean(ages))
+print(median(ages))
+print(mode(ages))
+print(small_boi(ages))
+print(big_boi(ages))
+print(removal(ages))
+print(odd_times(ages))
+print(even_times(ages))
+print(tell_me(ages))
+print(bonus(ages))
 
 
 
